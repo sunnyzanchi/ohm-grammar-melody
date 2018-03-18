a9a70abc04012f1f5809ecbba8dff50d8bc546f6
Zanchi <emzanchi@gmail.com>
2018-03-17 22:18:57 -0500

    Updated build config

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ export default {
   external: ['ohm-js'],
   input: 'src/index.js',
   output: {
-    file: 'index.js',
+    file: 'dist/index.js',
     format: 'cjs',
   },
   plugins: [inlinejs()],
